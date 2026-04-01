0637a01705eacd56dea400fdf61c77bb9a48979c
Shivak007 <kshivaoffl2824@gmail.com>
2026-04-01 21:24:46 +0530

    [Shiva Kumar] : Added : UC6 Lombok integration for DTO

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,13 +1,10 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import lombok.Data;
+
+@Data
 public class AddressBookDTO {
 
     private String name;
     private String city;
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
 }
